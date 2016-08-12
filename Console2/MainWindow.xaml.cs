1f2a31fcdc6a0ccb63f4905c9a8bb4665307484f
M.Seo <meongheeseo@gmail.com>
2016-08-12 17:25:30 +0900

    2016.08.12 Serial port operations

--- Console2/MainWindow.xaml.cs
+++ Console2/MainWindow.xaml.cs
@@ -22,12 +22,13 @@ namespace Console2
     /// </summary>
     public partial class MainWindow : Window
     {
-        int manufacturer_id = 1; // 1 - Shinwoo, 2 - Bombardier, 3 - Siemens
+        int manufacturer_id = 0; // 0 - Shinwoo, 1 - Bombardier, 2 - Siemens
+        int device_id = 0; // 0 - Balise, 2 - LEU
+        int func_id = 0; // 0 - ACK, 1 - NAK, 2 - Telegram Download, 3 - Telegram Upload
 
         public MainWindow()
         {
             InitializeComponent();
-            //msgbox.AppendText("HI", "Red");
             position_gpbox.Visibility = Visibility.Hidden;
             progressLabel.Text = String.Format("{0}%", progressBar.Value);
         }
@@ -41,7 +42,7 @@ namespace Console2
             try
             {
                 position_gpbox.Visibility = Visibility.Hidden;
-                manufacturer_id = 1;
+                manufacturer_id = 0;
             }
             catch (Exception) { }
         }
@@ -49,33 +50,31 @@ namespace Console2
         private void manu_bombardier_Checked(object sender, RoutedEventArgs e)
         {
             position_gpbox.Visibility = Visibility.Visible;
-            manufacturer_id = 2;
+            manufacturer_id = 1;
         }
 
         private void manu_siemens_Checked(object sender, RoutedEventArgs e)
         {
             position_gpbox.Visibility = Visibility.Hidden;
-            manufacturer_id = 3;
+            manufacturer_id = 2;
         }
         
         
         // ----- CHECKS IF BALISE IS SELECTED ----- //
-        Boolean balise_checked = true;
-
         private void pos_balise_Checked(object sender, RoutedEventArgs e)
         {
-            balise_checked = true;
+            device_id = 0;
         }
 
         private void pos_leu_Checked(object sender, RoutedEventArgs e)
         {
-            balise_checked = false;
+            device_id = 1;
         }
 
         // ----- READ, WRITE, RESET BUTTON CONTROL ----- //
         private void posread_btn_Click(object sender, RoutedEventArgs e)
         {
-            if (balise_checked)
+            if (device_id == 0)
             {
                 MessageBox.Show("Connect Balise Cable", "Warning", MessageBoxButton.OKCancel);
             }
@@ -87,7 +86,7 @@ namespace Console2
 
         private void poswrite_btn_Click(object sender, RoutedEventArgs e)
         {
-            if (balise_checked)
+            if (device_id == 0)
             {
                 MessageBox.Show("Connect Balise Cable", "Warning", MessageBoxButton.OKCancel);
             }
@@ -144,10 +143,13 @@ namespace Console2
             serialportbox.Items.Add(value);
             serialportbox.Items.SortDescriptions.Add(
                 new System.ComponentModel.SortDescription("", System.ComponentModel.ListSortDirection.Ascending));
-
+            
             // Connect the serial port.
             serialport = new SerialPort(value, 38400, Parity.None, 8, StopBits.One);
             serialport.Open();
+
+            if (serialport.IsOpen) { MessageBox.Show("Connection Open", "Message"); }
+            else { MessageBox.Show("Recheck Connection", "Warning"); }
         }
 
         // ----- PROGRESS BAR ----- //
@@ -158,25 +160,26 @@ namespace Console2
 
         private void download_btn_Click(object sender, RoutedEventArgs e)
         {
+            func_id = 2;
             String filter = "";
 
-            if (manufacturer_id == 1 && balise_checked)
+            if (manufacturer_id == 0 && device_id == 0)
             {
                 filter = "Shinwoo Balise File (*.BAL.ENC)|*.BAL.ENC|Shinwoo Balise File (*.ENC)|*.ENC";
             }
-            else if (manufacturer_id == 1 && !balise_checked)
+            else if (manufacturer_id == 0 && device_id == 1)
             {
                 filter = "Shinwoo LEU File (*.LEU.ENC)|*.LEU.ENC|Shinwoo LEU File (*.ENC)|*.ENC";
             }
-            else if (manufacturer_id == 2 && balise_checked)
+            else if (manufacturer_id == 1 && device_id == 0)
             {
                 filter = "Bombarider Balise File (*.BAL)|*.BAL";
             }
-            else if (manufacturer_id == 2 && !balise_checked)
+            else if (manufacturer_id == 1 && device_id == 1)
             {
                 filter = "Bombardier LEU File (*.LEU)|*.LEU";
             }
-            else if (manufacturer_id == 3 && balise_checked)
+            else if (manufacturer_id == 2 && device_id == 0)
             {
                 filter = "Siemens Balise File (*.TLG)|*.TLG";
             }
@@ -186,21 +189,75 @@ namespace Console2
             }
 
             byte[] data = openFile(filter);
-            int size = data.Length;
-            int printCount = 0;
 
-            while (printCount < size)
-            {
-                // Output file data in hex format
-                msgbox.AppendText(String.Format("{0:X}", data[printCount]));
-                printCount++;
-            }
+            SerialPortSend(data);
+        }
+
+        // Sends downloaded telegram message via serial port.
+        public void SerialPortSend(byte[] data)
+        { 
+            // Preamble
+            byte[] preamble = new byte[] { 0xAA, 0x55, 0x55, 0xAA };
+            
+            // Payload length
+            int payload_length = 4 + 4 + 1 + 4 + 2 + 2 + data.Length;
+            byte[] length = new byte[4];
+            length[0] = (byte)(payload_length >> 24);
+            length[1] = (byte)(payload_length >> 16);
+            length[2] = (byte)(payload_length >> 8);
+            length[3] = (byte)payload_length;
+
+            // Source Sequence Number
+            byte[] seq = new byte[] { 0x00, 0x01, 0x02, 0x03 };
+
+            // Destination Sequence Number
+            byte[] dest = new byte[] { 0x00, 0x00, 0x00, 0x00 };
+            
+            // Protocol version
+            byte[] version = new byte[] { 0x00 };
+
+            // Category
+            byte[] cat = new byte[4];
+            cat[0] = (byte)((1 << 6) + (manufacturer_id << 3) + ((func_id >> 2)));
+            cat[1] = (byte)((func_id << 6) + (device_id << 3));
+
+            // Record seq
+            byte[] record;
+            // if length of the message exceeds 1024 byte, send 0x00, 0x01
+            if (payload_length <= 1024) { record = new byte[] { 0x00, 0x00 }; }
+            else { record = new byte[] { 0x00, 0x01 }; }
+
+            // Data byte length
+            byte[] data_length = new byte[2];
+            data_length[0] = (byte)(data.Length >> 8);
+            data_length[1] = (byte)(data.Length);
+
+            // CRC32
+            byte[] crc32 = new byte[] { 0x00, 0x00, 0x00, 0x00 };
+
+            // postamble
+            byte[] postamble = new byte[] { 0x55, 0xAA, 0xAA, 0x55 };
+
+            serialport.Write(preamble, 0, preamble.Length);
+            serialport.Write(length, 0, length.Length);
+            serialport.Write(seq, 0, seq.Length);
+            serialport.Write(dest, 0, dest.Length);
+            serialport.Write(version, 0, version.Length);
+            serialport.Write(cat, 0, cat.Length);
+            serialport.Write(record, 0, record.Length); 
+            serialport.Write(data_length, 0, data_length.Length);
+            serialport.Write(data, 0, data.Length);
+            serialport.Write(crc32, 0, crc32.Length);
+            serialport.Write(postamble, 0, postamble.Length);
+
+            serialport.Close();
+            if (!serialport.IsOpen) { MessageBox.Show("Serial Port successfully closed.", "Message"); }
         }
 
         //
         // Open Telegram file and return read data.
         //
-        private byte[] openFile(String filter)
+        public byte[] openFile(String filter)
         {
             OpenFileDialog openFile = new OpenFileDialog();
             openFile.Filter = filter;
